3f5785173e8d16b124589eb8f52157c84ab18b5c
Ostrop <stepan.dihnich@mail.ru>
2023-05-22 00:38:06 +0300

    Add auth window

--- TimeTable/TimeTable/MainWindow.xaml.cs
+++ TimeTable/TimeTable/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,6 +16,16 @@ using System.Windows.Shapes;
 
 namespace TimeTable
 {
+    public class Phone
+    {
+        public string Title { get; set; }
+        public string buf1 { get; set; }
+        public string buf2 { get; set; }
+        public string buf3 { get; set; }
+        public string buf4 { get; set; }
+        public string buf5 { get; set; }
+        public string buf6 { get; set; }
+    }
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
@@ -23,6 +34,23 @@ namespace TimeTable
         public MainWindow()
         {
             InitializeComponent();
+            //MessageBox.Show("Вы уверены, что хотите выйти?", "Выход", MessageBoxButton.YesNo, MessageBoxImage.Stop);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //List<Phone> phonesList = new List<Phone>
+            //  {
+            //         new Phone { Title="25331/2", Company="Программирование", Price="09.01.2022" },
+            //         new Phone {Title="23643/5", Company="Робототехника", Price="09.01.2022" },
+            //        new Phone {Title="42312/1", Company="Электроника", Price="09.01.2022" }
+            //   };
+            //phonesGrid.ItemsSource = phonesList;
         }
     }
 }
---
+++ TimeTable/TimeTable/WPF/AuthorizationWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TimeTable.WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Функция отображения пароля
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SeePassword_Click(object sender, RoutedEventArgs e)
+        {
+            var SeePassword_checkBox = sender as CheckBox;
+            if (SeePassword_checkBox.IsChecked.Value)
+            {
+                PasswordTextBox.Text = PasswordBox.Password;
+                PasswordTextBox.Visibility = Visibility.Visible;
+                PasswordBox.Visibility = Visibility.Hidden;
+                SeePassword_checkBox.Content = Resources["Image.See"];
+            }
+            else
+            {
+                PasswordBox.Password = PasswordTextBox.Text;
+                PasswordBox.Visibility = Visibility.Visible;
+                PasswordTextBox.Visibility = Visibility.Hidden;
+                SeePassword_checkBox.Content = Resources["Image.SeeNot"];
+            }
+        }
+    }
+}
